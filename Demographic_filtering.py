860666ed7f61c0d59e9355610ffaa23eb7ed2eab
BhishmaGaudani <67182241+BhishmaGaudani@users.noreply.github.com>
2023-02-05 15:22:51 +0530

    Add files via upload

---
+++ Demographic_filtering.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import pandas as pd
+
+df=pd.read_csv("final.csv")
+C=df["vote_average"].mean()
+m=df["vote_count"].quatile(0.9)
+Q_movies=df.copy().loc[df["vote_count"]>=m]
+
+def weighted_rating(x,m=m,C=C):
+  v=x["vote_count"]
+  R=x["vote_average"]
+  return ((v/(v+m))*R)+((m/(v+m))*C)
+
+Q_movies["score"]=Q_movies.apply(weighted_rating,axis=1)
+
+Q_movies=Q_movies.sort_values("score",ascending=False)
+output=Q_movies[["original_title","poster_link","release_date","runtime","vote_average","overview"]].head(20).values.tolist()
+
